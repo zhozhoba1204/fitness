7e401fac58727951be877161b8d6a71981de5e64
Slava <blinov.slava@list.ru>
2022-04-06 10:10:12 +0300

    add data to table membership

---
+++ src/main/resources/db/migration/V4__Add_Data_Membership.sql
@@ -0,0 +1,7 @@
+INSERT INTO membership (end_date, section,user_id)
+VALUES (TO_DATE('01-12-2022','dd-MM-yyyy'), 'Boxing', 1),
+       (TO_DATE('01-12-2023','dd-MM-yyyy'), 'Yoga', 2),
+       (TO_DATE('01-11-2022','dd-MM-yyyy'), 'Gym', 1),
+       (TO_DATE('01-10-2022','dd-MM-yyyy'), 'Gym', 3),
+       (TO_DATE('01-11-2024','dd-MM-yyyy'), 'Swimming', 3),
+       (TO_DATE('07-11-2022','dd-MM-yyyy'), 'Boxing', 3);
